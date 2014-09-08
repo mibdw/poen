cf7211eb50cf1d07aabc43416fc0a2e73bb1e078
Ben van den Ende <ben@meedoenisbelangrijkerdanwinnen.nl>
2014-09-08 14:53:43 +0200

    hover color for events removed

--- public/scripts/poen-app.js
+++ public/scripts/poen-app.js
@@ -171,18 +171,14 @@ app.controller('poenGlobal', ['$scope', '$http',
 					$(this).addClass('active');			
 				},
 				eventMouseover: function(calEvent, jsEvent, view) {
-					var scope = angular.element('html').scope(); 
-					scope.$apply (function () {	
-						var hoverColor = scope.morphColor(calEvent.category.color, -30);
-						$('.' + calEvent._id).css('background-color', hoverColor);
-					});
+					$('.' + calEvent._id).css('background-color', 'black');
 				},
 				eventMouseout: function(calEvent, jsEvent, view) { 
 					$('.' + calEvent._id).css('background-color', calEvent.category.color);					
 				},
 				eventRender: function(event, element) {
 					element.css('background-color', event.category.color);
-					element.attr('title', event.title + ' [' + event.category.name + '] =' + event.amount)
+					element.attr('title', event.title + ' [' + event.category.name + '] = ' + event.amount)
 					element.addClass(event._id);
 					element.append('<span class="user" style=>' + event.user.username.substr(0, 1) + '</span>');	
 				}
@@ -393,25 +389,6 @@ app.controller('poenGlobal', ['$scope', '$http',
 				$('.calendar-view').fullCalendar('refetchEvents');
 				$scope.getCategories();
 			});
-		}
-
-		$scope.morphColor = function (col, amt) {
-			var usePound = false;
-			if (col[0] == "#") {
-				col = col.slice(1);
-				usePound = true;
-			}
-			var num = parseInt(col,16);
-			var r = (num >> 16) + amt;
-			if (r > 255) r = 255;
-			else if  (r < 0) r = 0;
-			var b = ((num >> 8) & 0x00FF) + amt;
-			if (b > 255) b = 255;
-			else if  (b < 0) b = 0;
-			var g = (num & 0x0000FF) + amt;
-			if (g > 255) g = 255;
-			else if (g < 0) g = 0;
-			return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
 		}
 	}
 ]);
